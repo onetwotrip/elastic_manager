b2d0e50eedb906d0bca4ae52867bf80e79215304
Antony Ryabov <anton.ryabov@onetwotrip.com>
2018-10-19 17:57:13 +0300

    splitted big method do_open

--- lib/elastic_manager.rb
+++ lib/elastic_manager.rb
@@ -23,70 +23,117 @@ class ElasticManager
     @elastic = Request::Elastic.new(@config)
   end
 
-  def work
+  def open_prechecks(indices, date_from, date_to)
+    unless date_from < date_to
+      log.fatal "wrong dates: date to is behind date from. from: #{date_from}, to: #{date_to}"
+      exit 1
+    end
+
+    unless true?(@config['force'])
+      unless @elastic.green?
+        log.fatal "elasticsearch on #{@config['es']['url']} is not green"
+        exit 1
+      end
+    end
+
+    if indices.length == 1 && indices.first == '_all'
+      indices = @elastic.all_indices(date_from, date_to, 'close')
+    end
+
+    indices
+  end
+
+  def skip_open?(index_name)
+    if @config['settings'][index_name]
+      if @config['settings'][index_name]['skip_open']
+        log.debug @config['settings'][index_name]['skip_open'].inspect
+
+        if true?(@config['settings'][index_name]['skip_open'])
+          log.warn "#{index_name} index open skiped"
+          return true
+        end
+      end
+    end
+
+    false
+  end
+
+  def index_exist?(response)
+    if response.code == 200
+      return true
+    elsif response.code == 404
+      return false
+    else
+      log.fatal "wtf in index_exist? response was: #{response.code} - #{response}"
+      exit 1
+    end
+  end
+
+  def already_open?(response)
+    if response.body.to_s =~ /open/
+      log.warn "#{index} index already opened"
+      return true
+    end
+
+    false
+  end
+
+  def open_prepare_vars
+    indices   = @config['indices'].split(',')
+    date_from = Date.parse(@config['from'])
+    date_to   = Date.parse(@config['to'])
+    indices   = open_prechecks(indices, date_from, date_to)
+
+    return indices, date_from, date_to
+  end
+
+  def open_index(index)
+    if @elastic.open_index(index)
+      log.info "#{index} index open success"
+    else
+      log.fatal "#{index} index open failed"
+    end
+  end
+
+  def restore_index(index)
+    if @elastic.restore_snapshot(index)
+      log.info "#{index} restored"
+    else
+      log.error "#{index} troubles with restore"
+    end
+  end
+
+  def open(indices, date)
+    indices.each do |index_name|
+      next if skip_open?(index_name)
+
+      index    = "#{index_name}-#{date}"
+      response = request(:get, "/_cat/indices/#{index}")
+
+      if index_exist?(response)
+        next if already_open?(response)
+
+        open_index(index)
+      else
+        log.warn "#{index} index not found"
+        log.info "#{index} trying snapshot restore"
+
+        restore_index(index)
+      end
+    end
+  end
+
+  def do_open
+    indices, date_from, date_to = open_prepare_vars
+
+    date_from.upto(date_to) do |date|
+      open(indices, date.to_s.tr!('-', '.'))
+    end
+  end
+
+  def run
     if @config['task'].downcase == 'open'
-      indices   = @config['indices'].split(',')
-      date_from = Date.parse(@config['from'])
-      date_to   = Date.parse(@config['to'])
-
-      unless true?(@config['force'])
-        unless @elastic.green?
-          log.fatal "elasticsearch on #{@config['es']['url']} is not green"
-          exit 1
-        end
-      end
-
-      if indices.length == 1 && indices.first == '_all'
-        # indices = es_all_indices(date_from, date_to, 'close')
-        indices = @elastic.all_indices(date_from, date_to, 'close')
-      end
-
-      date_from.upto(date_to) do |date|
-        date = date.to_s.tr!('-', '.')
-
-        indices.each do |index_name|
-          if @config['settings'][index_name]
-            if @config['settings'][index_name]['skip_open']
-              log.debug @config['settings'][index_name]['skip_open'].inspect
-
-              if true?(@config['settings'][index_name]['skip_open'])
-                log.warn "#{index_name} index open skiped"
-                next
-              end
-            end
-          end
-
-          index = "#{index_name}-#{date}"
-
-          response = @elastic.request(:get, "/_cat/indices/#{index}")
-
-          if response.code == 404
-            log.warn "#{index} index not found"
-            log.info "#{index} trying snapshot restore"
-
-            if @elastic.restore_snapshot(index)
-              log.info "#{index} restored"
-            else
-              log.error "#{index} troubles with restore"
-            end
-          elsif response.code == 200
-            if response.body.to_s =~ /open/
-              log.warn "#{index} index already opened"
-              next
-            end
-
-            if @elastic.open_index(index)
-              log.info "#{index} index open success"
-            else
-              log.fatal "#{index} index open failed"
-              exit 1
-            end
-          else
-            log.fatal "can't work with index #{index} response was: #{response.code} - #{response}"
-            exit 1
-          end
-        end
-      end
+      do_open
     end
   end
 end
